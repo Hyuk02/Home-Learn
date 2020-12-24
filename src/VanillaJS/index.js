70de0d1dc2f30a7e78db4bdbaf2c340b7a4661ad
ballack <ballack02@naver.com>
2020-12-24 22:55:04 +0900

    function calculator

--- src/VanillaJS/index.js
+++ src/VanillaJS/index.js
@@ -1,7 +1,34 @@
-function sayHello(name, age){
-    return `Hello ${name} you are ${age} years old`;
+const calculator = {
+    plus: function(a, b){
+        return a + b;
+    },
+    minus: function substract(a,b){
+        return a-b;
+    },
+    multiply: function multiple(a,b){
+        return a*b;
+    },
+    divide: function divide(a,b){
+        return a/b;
+    },
+    exponentiation: function expo(a,b){
+        return a**b;
+    },
+    remainder: function remainder(a,b){
+        return(a%b)
+    }
 }
 
-const greetJunHyeok = sayHello("JunHyeok", 23);
+const plus = calculator.plus(5, 5);
+const minus = calculator.minus(5, 5);
+const multiply = calculator.multiply(5, 5);
+const divide = calculator.divide(5, 5);
+const exponentiation = calculator.exponentiation(5, 5);
+const remainder = calculator.remainder(5, 5);
 
-console.log(greetJunHyeok);
+console.log(plus);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
+console.log(exponentiation);
+console.log(remainder);
