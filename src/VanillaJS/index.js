3f9c1a476b947f6e934917dc9ff97e25259c91c3
ballack <ballack02@naver.com>
2020-12-24 23:14:43 +0900

    document

--- src/VanillaJS/index.js
+++ src/VanillaJS/index.js
@@ -1,34 +1,8 @@
-const calculator = {
-    plus: function(a, b){
-        return a + b;
-    },
-    minus: function substract(a,b){
-        return a-b;
-    },
-    multiply: function multiple(a,b){
-        return a*b;
-    },
-    divide: function divide(a,b){
-        return a/b;
-    },
-    exponentiation: function expo(a,b){
-        return a**b;
-    },
-    remainder: function remainder(a,b){
-        return(a%b)
-    }
-}
+//const title = document.getElementById("title");
+const title = document.querySelector("#title");
 
-const plus = calculator.plus(5, 5);
-const minus = calculator.minus(5, 5);
-const multiply = calculator.multiply(5, 5);
-const divide = calculator.divide(5, 5);
-const exponentiation = calculator.exponentiation(5, 5);
-const remainder = calculator.remainder(5, 5);
+console.log(title);
 
-console.log(plus);
-console.log(minus);
-console.log(multiply);
-console.log(divide);
-console.log(exponentiation);
-console.log(remainder);
+title.innerHTML = "Hi ! from JS";
+title.style.color = 'red';
+document.title = 'I own you now'
